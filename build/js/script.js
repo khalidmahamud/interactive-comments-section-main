8c437a6b80792549e59de9095bdc3801a0619b8d
Khalid <mahamud.0.khalid@gmail.com>
2023-08-25 05:40:09 +0600

    updated index.html

--- build/js/script.js
+++ build/js/script.js
@@ -19,7 +19,7 @@ function createNewComment(className) {
 
     const html = `
           <div aria-owns=${currentUser.username} class=${className}>
-              <div>
+              <div class="w-full">
                 <!-- Card Header -->
                 <div class="card__header">
       
@@ -32,7 +32,7 @@ function createNewComment(className) {
                 <div class="card__content">
       
                   <p class="content">
-                    ${content.textContent}
+                    ${content.value}
                   </p>
       
                 </div>
@@ -53,7 +53,9 @@ function createNewComment(className) {
                 <button class="reply__btn"><svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg> Reply</button>
       
               </div>
-          </div>`
+          </div>
+          
+          <div class="replies"></div>`
 
           card.innerHTML = html;
           cardContainer.appendChild(card);
